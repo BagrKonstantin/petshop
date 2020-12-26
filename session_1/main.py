a081c6da55c7dfd3675aac03a14a44b5e3b315d1
Abusswalker <af55748@gmail.com>
2020-12-26 12:12:51 +0300

    sdsdlkmvmfvr

--- session_1/main.py
+++ session_1/main.py
@@ -1,7 +1,38 @@
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QTableWidgetItem, QLabel, QFileDialog
 from PyQt5 import QtWidgets, QtGui, QtCore
 from session_1.window_1 import Ui_MainWindow
+from session_1.save_window import Ui_MainWindow as SaveWin
 import sqlite3
+
+
+class Save(QMainWindow, SaveWin):
+    def __init__(self, path):
+        super(Save, self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle('enter data')
+        self.path = path
+        self.pushButton_save.clicked.connect(self.save_data)
+
+    def save_data(self):
+        try:
+            title = str(self.lineEdit_title.text())
+            pet = str(self.comboBox_animal.currentText())
+            type_p = str(self.comboBox_type.currentText())
+            p_price = float(self.lineEdit_purchasing_price.text())
+            r_price = float(self.lineEdit_retail_price.text())
+            description = str(self.textBrowser.toPlainText())
+            print(title, pet, type_p, description, p_price, r_price)
+
+            con = sqlite3.connect(self.path)
+            cur = con.cursor()
+            cur.execute(
+                """insert into products (title_products, pet, type, description, purchase_price, retail_price) VALUES 
+                    ("{}", "{}", "{}", "{}", {}, {})""".format(
+                    title, pet, type_p, description, p_price, r_price))
+            con.commit()
+            con.close()
+        except Exception as err:
+            print(err)
 
 
 class Win1(QMainWindow, Ui_MainWindow):
@@ -13,9 +44,8 @@ class Win1(QMainWindow, Ui_MainWindow):
 
         con = sqlite3.connect(self.path)
         cur = con.cursor()
-        self.data = cur.execute("""Select id_products, title_products, title_pet, title_type, description from products
-                                    join pet p on p.id_pet = products.id_pet
-                                    join type t on t.id_type = products.id_type""").fetchall()
+        self.data = cur.execute("""Select id_products, title_products, pet, type, description 
+                                    from products""").fetchall()
         con.close()
 
         self.tableWidget.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
@@ -24,15 +54,15 @@ class Win1(QMainWindow, Ui_MainWindow):
         self.tableWidget.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
 
         self.update_table()
-
+        self.pushButton_update.clicked.connect(self.update_data)
+        self.pushButton_add.clicked.connect(self.add)
         self.pushButton_update.clicked.connect(self.update_data)
 
     def update_data(self):
         con = sqlite3.connect(self.path)
         cur = con.cursor()
-        self.data = cur.execute("""Select id_products, title_products, title_pet, title_type, description from products
-                                    join pet p on p.id_pet = products.id_pet
-                                    join type t on t.id_type = products.id_type""").fetchall()
+        self.data = cur.execute(
+            """Select id_products, title_products, pet, type, description from products""").fetchall()
         con.close()
         self.update_table()
 
@@ -52,9 +82,14 @@ class Win1(QMainWindow, Ui_MainWindow):
             self.tableWidget.item(i, 2).setText(self.data[i][3])
             self.tableWidget.item(i, 3).setText(self.data[i][4])
 
+    def add(self):
+        self.win = Save(self.path)
+        self.win.show()
+
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = Win1("../bd.db")
     mainWindow.show()
